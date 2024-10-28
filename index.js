1f64fc1c6795f2c312aaf88e672c0248166289e0
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-10-28 22:22:26 +0500

    arrays

--- index.js
+++ index.js
@@ -10,16 +10,16 @@
 //     str=''
     
 // }
-let str=''
-for (let i = 0; i < 6; i++) {
-   for (let j = i; j < 5; j++) {
-   str = str+'s'
+// let str=''
+// for (let i = 0; i < 6; i++) {
+//    for (let j = i; j < 5; j++) {
+//    str = str+'s'
     
-   }
-    console.log(str);
-    str=''
+//    }
+//     console.log(str);
+//     str=''
     
-}
+// }
 
 
 
@@ -46,6 +46,57 @@ for (let i = 0; i < 6; i++) {
 
 // ------------ARRAYS-----------
 
-// -------maximum Number-------
+// -------Negative Number-------
 
-//
+// let arr = [3, -1, 5, -9, 0, -7, 8, -4];
+// for (let index = 0; index < arr.length; index++) {
+//     if (arr[index]<0) {
+//         console.log(arr[index]);
+        
+//     }
+    
+// }
+// -----------Positive Numbers-----------
+
+// let arr = [3, -1, 5, -9, 0, -7, 8, -4];
+// for (let index = 0; index < arr.length; index++) {
+//     if (arr[index]>0) {
+//         console.log(arr[index]);
+        
+//     }
+    
+// }
+
+// ---------sum of Array----------
+
+// let arr = [3, 1, 5, 9, 7, 8, 4];
+// const sum = arr.reduce((cru,next) =>(cru+next))
+// console.log(sum);
+
+
+
+// ----------Count Negetive Elements--------
+
+// let array = [4, -1, -3, 7, 8, 2];
+// const countNegative=()=>{
+    
+//     let Count=0
+//    for (let i = 0; i < array.length; i++) {
+//     if (array[i]%0) {
+//       Count=  Count+1;
+//     }
+    
+//    }
+//    return Count
+
+// }
+// console.log(countNegative(array));
+
+
+// ------another method--------
+
+// let array = [4, -1, 3, 7, 8, 2];
+// const countNegative=()=>{
+//     return array.filter((element)=>element<0).length
+// }
+// console.log(countNegative(array));

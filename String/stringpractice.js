28e19974e3cb4e5195c20760d1657d15296ec7f8
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-11-18 23:47:35 +0500

    commit string

---
+++ String/stringpractice.js
@@ -0,0 +1,16 @@
+// let fruits = ["Mango", "Banana", "Orange", "Pineapple"];
+// let someFruits = fruits.slice(1, 3);
+// console.log(someFruits);
+
+
+// let fruits = ["Mango", "Banana", "Orange", "Pineapple"];
+// fruits.splice(0, 1, "Bear");
+// console.log(fruits);
+
+
+// let name = "Hassan";
+// let newName = name.slice(1, 5);
+//   name.splice(1, 1, "B");
+// name.pop();
+// console.log(name);
+// console.log(newName);

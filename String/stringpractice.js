ac5d6d6a66bf8df2ebbff7c9016a054472306495
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-11-19 23:52:01 +0500

    string

--- String/stringpractice.js
+++ String/stringpractice.js
@@ -14,3 +14,50 @@
 // name.pop();
 // console.log(name);
 // console.log(newName);
+
+let fruits = ["Mango", "Banana", "Orange", "Pineapple"];
+// let newFruits = fruits.slice(0).concat("abc");
+let newFruits = [].concat(fruits, "abc", "an");
+// let newFruits = [...fruits, "newElements"];
+fruits.push("Fruits");
+console.log(fruits);
+console.log(newFruits);
+
+// let i = 0;
+// while(i < fruits.length){
+//     console.log(fruits[i]);
+//     i++;
+// }
+
+// for (let i = 0; i < fruits.length; i++) {
+//     const element = fruits[i];
+//     console.log(element);
+    
+// }
+
+// let fruits = ["Mango", "Banana", "Orange", "Pineapple"];
+// fruits = ["Dates", "Mango"];
+// fruits.push("Dates");
+// console.log(fruits);
+
+
+// let fruits = ["Mango", "Banana", "Orange", "Pineapple"];
+
+// for(let fruit of fruits){
+//     // console.log(a);
+// }
+
+// for(let index in fruits){
+//     console.log(index, fruits[index]);
+// }
+
+
+// let laptops = ["HP", "MAC"];
+
+// let hp = laptops[0];
+// let mac = laptops[1];
+
+// let [hp, mac, dell] = laptops;
+
+
+// console.log(hp, mac, dell)

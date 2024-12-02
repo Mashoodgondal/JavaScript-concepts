1100b7f85361f68ae0eb9616c4379bef73efa1eb
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-12-02 22:57:31 +0500

    date

--- Date/index.js
+++ Date/index.js
@@ -9,3 +9,13 @@ var monthdif = milisecondAge/(1000*60*60*24*365*12)
 var day = milisecondAge/(1000*60*60*24*365*12*30)
 
 console.log(`i'm  ${yearDif.toFixed(0)} years ${monthdif.toFixed(0)} months and ${day.toFixed(0)} days now `);
+
+var dob = new Date("01 Aug,2005")
+var date = new Date()
+
+var milisecondAge = date.getTime()- dob.getTime()
+var yearDif = milisecondAge/(1000*60*60*24*365)
+var monthdif = milisecondAge/(1000*60*60*24*365*12)
+var day = milisecondAge/(1000*60*60*24*365*12*30)
+
+console.log(`i'm  ${yearDif.toFixed(0)} years ${monthdif.toFixed(0)} months and ${day.toFixed(0)} days now `);

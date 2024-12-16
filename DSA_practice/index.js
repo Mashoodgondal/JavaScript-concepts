79263936c43e506e7c924eea6a8efed9508f69da
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-12-16 20:57:19 +0500

    DSA

---
+++ DSA_practice/index.js
@@ -0,0 +1,30 @@
+// 1---- Find the Maximum Number in an Array
+// let arr = [2, 4, 15, 34, 6, 4, 56]
+// let maxNum = arr.reduce((previous, curront) => previous > curront ? previous : curront)
+// console.log(maxNum);
+
+// 2--------- Check if a String is a Palindrome
+// let str = '3234'
+// function reverseString(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed === str;
+// }
+
+
+// let rev = reverseString(str);
+// if (rev === true) {
+//     console.log("Palindrome");
+
+// }
+// else {
+//     console.log("Non Palindrome");
+
+// }
+
+// 3. Sum of Elements in an Array------------
+// let arr = [2, 4, 15, 34, 6, 4, 56]
+// let sum = arr.reduce((pre, cru) => pre + cru, 0)
+// console.log(sum);

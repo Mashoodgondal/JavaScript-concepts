cbf9b429cc2d1c82298321f8a0f7e8075a83a9ef
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-02 22:22:39 +0500

    area calculation

--- DSA_practice/index.js
+++ DSA_practice/index.js
@@ -134,23 +134,43 @@
 //  with the x-axis form a container, such that the container contains the most water.
 //                    ------------------------
 
-var maxArea = function (height) {
-    let left = 0
-    let right = height.length - 1
-    let maxWater = 0
-    while (left < right) {
-        let minHight = Math.min(height[left], height[right])
-        let width = right - left
-        let area = minHight * width
-        maxWater = Math.max(maxWater, area)
-        if (height[left] < height[right]) {
-            left++;
-        }
-        else {
-            right--
-        }
-    }
-    return maxWater
-};
-console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+// var maxArea = function (height) {
+//     let left = 0
+//     let right = height.length - 1
+//     let maxWater = 0
+//     while (left < right) {
+//         let minHight = Math.min(height[left], height[right])
+//         let width = right - left
+//         let area = minHight * width
+//         maxWater = Math.max(maxWater, area)
+//         if (height[left] < height[right]) {
+//             left++;
+//         }
+//         else {
+//             right--
+//         }
+//     }
+//     return maxWater
+// };
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
 // console.log("Good thing is good");
+// ==================
+// var maxArea = function (height) {
+//     let left = 0
+//     let right = height.length - 1
+//     let maxWater = 0
+//     while (left < right) {
+//         let minHight = Math.min(height[left], height[right])
+//         let width = right - left
+//         let area = minHight * width
+//         maxWater = Math.max(maxWater, area)
+//         if (height[left] < height[right]) {
+//             left++;
+//         }
+//         else {
+//             right--
+//         }
+//     }
+//     return maxWater
+// };
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));

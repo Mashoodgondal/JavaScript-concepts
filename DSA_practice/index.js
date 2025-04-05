78f62f5cc1b66aa23208466d5419a1751f0411e3
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-05 23:47:47 +0500

    commit

--- DSA_practice/index.js
+++ DSA_practice/index.js
@@ -265,4 +265,28 @@ var removeElement = function (nums, val) {
 
     return k; // ✅ Return number of remaining elements
 };
+var removeElement = function (nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k; // ✅ Return number of remaining elements
+};
+var removeElement = function (nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k; // ✅ Return number of remaining elements
+};
 

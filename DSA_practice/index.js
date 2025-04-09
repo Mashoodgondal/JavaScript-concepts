9de2b1121df1d7485fe7d5bfcae0219ab66e6af9
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-09 23:12:10 +0500

    commit

--- DSA_practice/index.js
+++ DSA_practice/index.js
@@ -316,3 +316,15 @@ var removeDuplicates = function (nums) {
     return k
 }
 console.log(removeDuplicates(1, 1, 2));
+
+var removeDuplicates = function (nums) {
+    let k = 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[k - 1]) {
+            nums[k] = nums[i]
+            k++
+        }
+    }
+    return k
+}
+console.log(removeDuplicates(1, 1, 2));

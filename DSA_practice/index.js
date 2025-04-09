f253c36b63648885257283ffde5f7a1aa85ecdcf
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-09 23:11:24 +0500

    commit

--- DSA_practice/index.js
+++ DSA_practice/index.js
@@ -293,9 +293,21 @@
 // }
 // console.log(removeDuplicates([1, 1, 2]));
 
+// var removeDuplicates = function (nums) {
+//     let k = 1
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i] !== nums[k - 1]) {
+//             nums[k] = nums[i]
+//             k++
+//         }
+//     }
+//     return k
+// }
+// console.log(removeDuplicates(1, 1, 2));
+
 var removeDuplicates = function (nums) {
     let k = 1
-    for (let i = 0; i <= nums.length; i++) {
+    for (let i = 1; i < nums.length; i++) {
         if (nums[i] !== nums[k - 1]) {
             nums[k] = nums[i]
             k++

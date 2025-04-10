f0aac9c37fac2589704c91acf15e2399862e297e
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-10 23:46:10 +0500

    try

--- DSA_practice/index.js
+++ DSA_practice/index.js
@@ -319,3 +319,13 @@ var printvar = function (nums, k, num1, m) {
     return [...narr, ...narr1]
 }
 console.log(printvar([1, 2, 3, 4], 2, [7, 6, 9, 4], 2));
+let narr = []
+let narr1 = []
+for (let i = 0; i <= k; i++) {
+    narr.push(nums[i])
+
+}
+for (let i = 0; i <= m; i++) {
+    narr1.push(num1[i])
+}
+return [...narr, ...narr1]

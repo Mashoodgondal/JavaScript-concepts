8138f48856a30f587c4cf6af15a3454ed4b73fff
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-10 23:10:00 +0500

    dsa

--- DSA_practice/index.js
+++ DSA_practice/index.js
@@ -251,7 +251,7 @@
 //         }
 //     }
 
-//     return k; // ✅ Return number of remaining elements
+//     return k;
 // };
 //                     --------------
 //     Input: nums = [1, 1, 2]
@@ -305,26 +305,14 @@
 // }
 // console.log(removeDuplicates(1, 1, 2));
 
-var removeDuplicates = function (nums) {
-    let k = 1
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[k - 1]) {
-            nums[k] = nums[i]
-            k++
-        }
-    }
-    return k
-}
-console.log(removeDuplicates(1, 1, 2));
 
-var removeDuplicates = function (nums) {
-    let k = 1
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[k - 1]) {
-            nums[k] = nums[i]
-            k++
-        }
+var printvar = function (nums, k) {
+    // return nums.length - 1
+    let k = 0
+    for (let i = 0; i <= k; i++) {
+        return nums
+
     }
-    return k
+
 }
-console.log(removeDuplicates(1, 1, 2));
+console.log(printvar([1, 2, 3, 4], k));

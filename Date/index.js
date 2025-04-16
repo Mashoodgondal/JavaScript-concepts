c116cd290a59a384e7aa4e6d6edf4668065324eb
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-16 23:20:59 +0500

    date setting

--- Date/index.js
+++ Date/index.js
@@ -94,3 +94,27 @@ console.log("Date:", rightNow.getDate());
 // Get the full year
 console.log("Year:", rightNow.getFullYear());
 
+// Create a Date object for the current date and time
+var rightNow = new Date();
+
+// Get the day of the week as a number (0 for Sunday, 1 for Monday, etc.)
+var dayNumber = rightNow.getDay();
+
+// Array to map day numbers to day names
+var arr = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get the name of the day from the array
+var dayName = arr[dayNumber];
+console.log("Today is", dayName);
+
+// Get the current date
+console.log("Date:", rightNow.getDate());
+
+// Get the full year
+console.log("Year:", rightNow.getFullYear());
+
+// var milisecondAge = date.getTime()- dob.getTime()
+// // var yearDif = milisecondAge/(1000*60*60*24*365)
+// var yearDif = date.getFullYear()-dob.getFullYear()
+// var monthdif = date.getMonth()-dob.getMonth()
+// var day =   date.getDay()-dob.getDay()

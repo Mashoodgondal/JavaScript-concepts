b76fd6c0a466dfa8a2f9704043fb63a56485b7a5
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-04-22 20:17:09 +0500

    two sum

--- DSA_practice/index2.js
+++ DSA_practice/index2.js
@@ -2,12 +2,35 @@
 // ===============   the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length
 //                     and return it.
 
-var buildArray = function (nums) {
-    let ans = [];
-    for (let i = 0; i < nums.length; i++) {
-        ans.push(nums[nums[i]])
+// var buildArray = function (nums) {
+//     let ans = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         ans.push(nums[nums[i]])
 
+//     }
+//     return ans
+// };
+// console.log(buildArray([0, 2, 1, 5, 3, 4]));
+
+// -------------Two sum ---------------
+var twoSum = function (nums, target) {
+    // const map = new Map()
+    // for (let i = 0; i <= nums.length; i++) {
+    //     const complemnt = target - nums[i]
+    //     if (map.has(complemnt)) {
+    //         return [map.get(complemnt), i]
+    //     }
+    //     map.set(nums[i], i)
+
+    // }
+    // return [];
+    for (let i = 0; i <= nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [nums[i], nums[j]]
+            }
+        }
     }
-    return ans
+
 };
-console.log(buildArray([0, 2, 1, 5, 3, 4]));
+console.log(twoSum([1, 3, 4, 6, 7, 9], 10));

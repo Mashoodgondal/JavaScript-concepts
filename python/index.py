4d93ced198beaf9f6e61dc52fbc297a64ad6039f
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-08 22:28:34 +0500

    weight calculation

--- python/index.py
+++ python/index.py
@@ -1,4 +1,7 @@
-for i in range(5):
-    print(i)
-
-    
+# for i in range(5):
+#     print(i)
+MARS_GRAVITY= 0.38
+earth_weight=input("Enter earth weight in kg: ")
+earth_weighted= float(earth_weight)
+mars_weight= earth_weighted * MARS_GRAVITY
+print(f"mars weight: {mars_weight}")

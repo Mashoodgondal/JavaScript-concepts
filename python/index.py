6f791fa423dc59d10247c40451fa9c26dfad1796
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-05-08 22:29:52 +0500

    2nd python

--- python/index.py
+++ python/index.py
@@ -1,5 +1,11 @@
 # for i in range(5):
 #     print(i)
+# MARS_GRAVITY= 0.38
+# earth_weight=input("Enter earth weight in kg: ")
+# earth_weighted= float(earth_weight)
+# mars_weight= earth_weighted * MARS_GRAVITY
+# print(f"mars weight: {mars_weight}")
+
 MARS_GRAVITY= 0.38
 earth_weight=input("Enter earth weight in kg: ")
 earth_weighted= float(earth_weight)

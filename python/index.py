80fc707ccb78509b77891758356f5170468d3be2
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-07-11 21:58:36 +0500

    commit

--- python/index.py
+++ python/index.py
@@ -120,3 +120,24 @@
 # for planet,ratio in gravity_ratios.items():
 #     planet_weight = earth_weight * ratio
 #     print(f"{planet}:{planet_weight:.3f}kg")
+# print("Weight of the object on other planets:")
+# for planet,ratio in gravity_ratios.items():
+#     planet_weight = earth_weight * ratio
+#     print(f"{planet}:{planet_weight:.3f}kg")
+
+# earth_weight = input("Enter Earth weight  ")
+# earth_weight= float(earth_weight)
+# gravity_ratios={
+#     "Mercury": 0.38,
+#         "Venus": 0.91,
+#         "Mars": 0.38,
+#         "Jupiter": 2.34,
+#         "Saturn": 1.06,
+#         "Uranus": 0.92,
+#         "Neptune": 1.19,
+#         "Pluto": 0.06
+# }
+# print("Weight of the object on other planets:")
+# for planet,ratio in gravity_ratios.items():
+#     planet_weight = earth_weight * ratio
+#     print(f"{planet}:{planet_weight:.3f}kg")

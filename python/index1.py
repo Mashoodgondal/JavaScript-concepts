ef94440874bc2a4ac31d867d20efcac1e24109d6
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-12 22:43:55 +0500

    function in python

--- python/index1.py
+++ python/index1.py
@@ -8,6 +8,28 @@
 # print(result)
 
 # FUNCTOIN TO FIND POWER
-def find_power(a,b):
-    return a ** b
-print(find_power(4,5))
+# def find_power(a,b):
+#     return a ** b
+# print(find_power(4,5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Function with multiple parameters
+# def add_numbers(a, b):  # a and b are parameters
+#     return a + b
+
+# # Calling with arguments
+# result = add_numbers(5, 3)  # 5 and 3 are arguments
+# print(result)  # Output: 8

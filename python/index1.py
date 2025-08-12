1759cf2090cd7e1fc6cc7cffb4f3103456de3a36
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-12 22:30:49 +0500

    function in python

---
+++ python/index1.py
@@ -0,0 +1,10 @@
+# STEP BY STEP ASSIGNMENT TO LEARN PYTHON
+
+
+# Function with multiple parameters
+def add_numbers(a, b):  # a and b are parameters
+    return a + b
+
+# Calling with arguments
+result = add_numbers(5, 3)  # 5 and 3 are arguments
+print(result)  # Output: 8

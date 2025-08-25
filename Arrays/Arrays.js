7cabd01bfda4583f73e76e46f9c77d941dde359d
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-25 22:39:40 +0500

    array dsa practice

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -697,4 +697,19 @@ console.log(deluser([1, 2, 3, 4, 5], 2));
 //         }
 
 //     }
+// return -1/ const binarySearch=(arr,target)=>{
+//     let left=0
+//     let right = arr.length-1
+//     while (left<=right) {
+//         const mid = Math.floor((left+right)/2)
+//         if (arr[mid]===target) {
+//             return mid
+//         }
+//         else if(arr[mid]<target){left= mid +1}
+//         else{
+
+//             right=  mid-1
+//         }
+
+//     }
 // return -1

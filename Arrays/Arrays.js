837cd7ba44fee6541c08ee1a36e19d716102e811
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-31 22:04:57 +0500

    practice revision

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -611,3 +611,43 @@ console.log(deluser([1, 2, 3, 4, 5], 2));
 // }
 // console.log(sortedArray);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let sortedArray = array.sort((a,b)=>a-b)
+// const binarySearch=(arr,target)=>{
+//     let left=0
+//     let right = arr.length-1
+//     while (left<=right) {
+//         const mid = Math.floor((left+right)/2)
+//         if (arr[mid]===target) {
+//             return mid
+//         }
+//         else if(arr[mid]<target){left= mid +1}
+//         else{
+
+//             right=  mid-1
+//         }
+
+//     }
+// return -1
+// }
+// console.log(sortedArray);
+

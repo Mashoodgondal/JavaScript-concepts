c775150a6c2f26ea07875f63fe6f9605a71b5121
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-08-31 22:05:28 +0500

    practice revision

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -616,38 +616,3 @@ console.log(deluser([1, 2, 3, 4, 5], 2));
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let sortedArray = array.sort((a,b)=>a-b)
-// const binarySearch=(arr,target)=>{
-//     let left=0
-//     let right = arr.length-1
-//     while (left<=right) {
-//         const mid = Math.floor((left+right)/2)
-//         if (arr[mid]===target) {
-//             return mid
-//         }
-//         else if(arr[mid]<target){left= mid +1}
-//         else{
-
-//             right=  mid-1
-//         }
-
-//     }
-// return -1
-// }
-// console.log(sortedArray);
-

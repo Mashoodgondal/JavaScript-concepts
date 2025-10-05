16b5d360c616721babcabbd9a9984332030149ab
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-10-05 20:38:02 +0500

    python problems

--- python/index.py
+++ python/index.py
@@ -40,12 +40,12 @@
 # print(f"factorial is {other}")
 
 
-# def fib(n):
-#     if n <= 1:
-#         return n
-#     return fib(n-1) + fib(n-2)
-# for i in range(10):
-#  print(fib(i), end=" ")
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+for i in range(10):
+ print(fib(10), end=" ")
 
 
 

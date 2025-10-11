54ca7a453ff3ef2830453147f77b1f04f721fa53
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-10-11 13:55:26 +0500

    add string

--- String/stringpractice.js
+++ String/stringpractice.js
@@ -304,3 +304,20 @@ console.log(newName);
 
 // }
 // someDetails(person)
+
+
+
+
+// someDetails(person)
+// let person= {
+//     name:'ali',
+//     age:'43',
+//     gender:'male',
+//     address:'fsd'
+// }
+
+// const someDetails=({name,age})=>{
+//     console.log(name,age);
+
+// }
+// someDetails(person)
